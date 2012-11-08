52bbdc463bbbc8fefc10a94d2c810dcb9efe7d80
Makara Wang <makara15@gmail.com>
2012-11-08 14:27:22 +0800

    So the demo server can handle dependencies.

--- servers/Resource.bones.js
+++ servers/Resource.bones.js
@@ -1,16 +1,28 @@
 // A demo server with only a set of helpers.
 server = Bones.Server.extend({
-    mount: function(name) {
+    initialize: function(app) {
+        this.mounted = {};
+    },
+    mount: function(name, route) {
         if (this.plugin.resources && this.plugin.resources[name]) {
-            var resource = new this.plugin.resources[name]();
-            resource.mountTo(this);
+            route || (route = '/');
+            // Don't mount a same resource to a same route twice or more.
+            this.mounted[route] || (this.mounted[route] = {});
+            if (!this.mounted[route][name]) {
+                var resource = new this.plugin.resources[name]();
+                // Handle dependencies.
+                _(resource.dependencies || null).each(function(name, route) {
+                    this.mount(name, route);
+                }, this);
+                this.mounted[route][name] = true;
+                resource.mountTo(this, route);
+            }
         }
         return this;
     },
     mountAll: function() {
-        _(this.plugin.resources || null).each(function(Res) {
-            var resource = new Res();
-            resource.mountTo(this);
+        _(this.plugin.resources || null).each(function(Res, name) {
+            this.mount(name);
         }, this);
         return this;
     }
